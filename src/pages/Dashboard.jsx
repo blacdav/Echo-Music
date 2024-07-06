84e8ea7461b106a32e245c35a52f3b4c6c1e63b2
blacdav <davidaniefoik@gmail.com>
2024-07-06 23:29:14 +0100

    designed the Trending Components and Top Playlist Components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Header from '../components/Header'
 import Recent from '../components/Recent'
+import Trending from '../components/Trending'
+import TopPlaylists from '../components/TopPlaylist'
 
 const Dashboard = () => {
   return (
@@ -13,8 +15,12 @@ const Dashboard = () => {
         <div>
             <Header />
         </div>
-        <div className='h-1/3'>
+        <div>
             <Recent />
+            <div className='flex'>
+                <div className='w-2/5'><Trending /></div>
+                <div className='w-full'><TopPlaylists /></div>
+            </div>
         </div>
       </div>
     </main>
